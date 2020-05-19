5728d63c0fe1af0911b3fc6cb18612e112e1d094
Wataru Matsui <watawuwu@3bi.tech>
2020-05-19 17:37:11 +0900

    feat: update ccclog ver of github action to 0.3

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -2,7 +2,8 @@
 
 repo_path="$1"
 revision_spec="$2"
+options="$3"
 
-changelog=$(ccclog  $repo_path $revision_spec)
+changelog=$(ccclog  $repo_path $revision_spec $options)
 changelog="${changelog//$'\n'/'%0A'}"
 echo "::set-output name=changelog::$changelog"
